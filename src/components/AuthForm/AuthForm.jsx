4deccf352b006897ec27d9fcfafc420af03922ab
mike10911 <mikeymundia@gmail.com>
2024-02-03 20:23:42 -0500

    role options on signin

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -11,7 +11,7 @@ const AuthForm = () => {
 		<>
 			<Box border={"1px solid gray"} borderRadius={4} padding={5}>
 				<VStack spacing={4}>
-					<Image src='/logo.png' h={24} cursor={"pointer"} alt='Instagram' />
+					<Image src='/logoLarge.png' h={10}cursor={"pointer"} alt='Nebula' />
 
 					{isLogin ? <Login /> : <Signup />}
 
--- src/components/AuthForm/Signup.jsx
+++ src/components/AuthForm/Signup.jsx
@@ -1,79 +1,103 @@
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { Alert, AlertIcon, Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertIcon,
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import useSignUpWithEmailAndPassword from "../../hooks/useSignUpWithEmailAndPassword";
+import { Radio, RadioGroup, Stack, Text } from "@chakra-ui/react";
 
 const Signup = () => {
-	const [inputs, setInputs] = useState({
-		fullName: "",
-		username: "",
-		email: "",
-		password: "",
-	});
-	const [showPassword, setShowPassword] = useState(false);
-	const { loading, error, signup } = useSignUpWithEmailAndPassword();
+  const [inputs, setInputs] = useState({
+    fullName: "",
+    username: "",
+    email: "",
+    password: "",
+	role: ""
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const { loading, error, signup } = useSignUpWithEmailAndPassword();
 
-	return (
-		<>
-			<Input
-				placeholder='Email'
-				fontSize={14}
-				type='email'
-				size={"sm"}
-				value={inputs.email}
-				onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
-			/>
-			<Input
-				placeholder='Username'
-				fontSize={14}
-				type='text'
-				size={"sm"}
-				value={inputs.username}
-				onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
-			/>
-			<Input
-				placeholder='Full Name'
-				fontSize={14}
-				type='text'
-				size={"sm"}
-				value={inputs.fullName}
-				onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
-			/>
-			<InputGroup>
-				<Input
-					placeholder='Password'
-					fontSize={14}
-					type={showPassword ? "text" : "password"}
-					value={inputs.password}
-					size={"sm"}
-					onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-				/>
-				<InputRightElement h='full'>
-					<Button variant={"ghost"} size={"sm"} onClick={() => setShowPassword(!showPassword)}>
-						{showPassword ? <ViewIcon /> : <ViewOffIcon />}
-					</Button>
-				</InputRightElement>
-			</InputGroup>
+  return (
+    <>
+      <Input
+        placeholder="Email"
+        fontSize={14}
+        type="email"
+        size={"sm"}
+        value={inputs.email}
+        onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+      />
+      <Input
+        placeholder="Username"
+        fontSize={14}
+        type="text"
+        size={"sm"}
+        value={inputs.username}
+        onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+      />
+      <Input
+        placeholder="Full Name"
+        fontSize={14}
+        type="text"
+        size={"sm"}
+        value={inputs.fullName}
+        onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+      />
+      <InputGroup>
+        <Input
+          placeholder="Password"
+          fontSize={14}
+          type={showPassword ? "text" : "password"}
+          value={inputs.password}
+          size={"sm"}
+          onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+        />
+        <InputRightElement h="full">
+          <Button
+            variant={"ghost"}
+            size={"sm"}
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      <Text fontSize={14} color={"gray.500"}>
+        Select your role
+      </Text>
+      <RadioGroup onChange={(e)=> setInputs({...inputs, role: e.target.value})}>
+        <Stack direction="row">
+          <Radio value="Enthusist">Enthusist</Radio>
+          <Radio value="Student">Student</Radio>
+          <Radio value="Educator">Educator</Radio>
+          <Radio value="Reseacher">Researcher</Radio>
+        </Stack>
+      </RadioGroup>
 
-			{error && (
-				<Alert status='error' fontSize={13} p={2} borderRadius={4}>
-					<AlertIcon fontSize={12} />
-					{error.message}
-				</Alert>
-			)}
+      {error && (
+        <Alert status="error" fontSize={13} p={2} borderRadius={4}>
+          <AlertIcon fontSize={12} />
+          {error.message}
+        </Alert>
+      )}
 
-			<Button
-				w={"full"}
-				colorScheme='blue'
-				size={"sm"}
-				fontSize={14}
-				isLoading={loading}
-				onClick={() => signup(inputs)}
-			>
-				Sign Up
-			</Button>
-		</>
-	);
+      <Button
+        w={"full"}
+        colorScheme="blue"
+        size={"sm"}
+        fontSize={14}
+        isLoading={loading}
+        onClick={() => signup(inputs)}
+      >
+        Sign Up
+      </Button>
+    </>
+  );
 };
 
 export default Signup;
